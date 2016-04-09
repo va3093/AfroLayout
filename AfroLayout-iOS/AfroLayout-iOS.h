8721bfeb42abf4d0773b123f245db762005e0f25
Wilhelm <va3093@gmail.com>
2016-04-09 11:45:41 +0100

    Added iOS framework target

---
+++ AfroLayout-iOS/AfroLayout-iOS.h
@@ -0,0 +1,19 @@
+//
+//  AfroLayout-iOS.h
+//  AfroLayout-iOS
+//
+//  Created by Wilhelm Van Der Walt on 4/9/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AfroLayout-iOS.
+FOUNDATION_EXPORT double AfroLayout_iOSVersionNumber;
+
+//! Project version string for AfroLayout-iOS.
+FOUNDATION_EXPORT const unsigned char AfroLayout_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AfroLayout_iOS/PublicHeader.h>
+
+
